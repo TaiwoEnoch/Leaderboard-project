1664afb4afd73c3d965639f2c897d05e6a0049da
TaiwoEnoch <taiwoenoch0@gmail.com>
2023-03-18 18:33:32 +0100

    fetch api

---
+++ src/modules/addScores.js
@@ -0,0 +1,10 @@
+export const sendScores = async (url, data) => {
+  const SCORES = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+  return SCORES;
+}
